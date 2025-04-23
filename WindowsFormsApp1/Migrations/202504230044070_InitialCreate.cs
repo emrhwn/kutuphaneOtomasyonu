00a6968df785acc2700923dd5b340696945145aa
emrhwn <emrhwn@gmail.com>
2025-04-23 03:58:21 +0300

    Kitap.cs ve KutuphaneContext.cs sınıfları oluşturuldu  App.config dosyasına EF ayarları ve bağlantı dizesi eklendi  InitialCreate migration oluşturuldu ve Update-Database komutuyla veritabanı oluşturuldu  Form tasarımı geliştirildi, bileşenler yerleştirildi ve bazı örnek veriler yüklendi (cmbAuthor, cmbStatus, nudYear)

---
+++ WindowsFormsApp1/Migrations/202504230044070_InitialCreate.cs
@@ -0,0 +1,30 @@
+﻿namespace WindowsFormsApp1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Kitaps",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        ISBN = c.String(nullable: false, maxLength: 20),
+                        Baslik = c.String(nullable: false, maxLength: 100),
+                        Yazar = c.String(nullable: false, maxLength: 50),
+                        Yil = c.Int(nullable: false),
+                        Durum = c.String(nullable: false, maxLength: 20),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Kitaps");
+        }
+    }
+}
---
+++ WindowsFormsApp1/Models/KutuphaneContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace WindowsFormsApp1.Models
+{
+    public class KutuphaneContext : DbContext
+    {
+        public KutuphaneContext() : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Kitap> Kitaplar { get; set; }
+    }
+}
